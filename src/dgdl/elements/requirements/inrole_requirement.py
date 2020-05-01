7d6d3151badfbc491457b6d0bd731f1558787374
Mark Snaith <msnaith@dundee.ac.uk>
2020-05-01 17:01:27 +0100

    Bug fix in role requirement

--- src/dgdl/elements/requirements/inrole_requirement.py
+++ src/dgdl/elements/requirements/inrole_requirement.py
@@ -2,8 +2,8 @@ from .requirement import Requirement
 
 class InRoleRequirement(Requirement):
 
-    def __init__(self, playerID, role, negative=False):
+    def __init__(self, negated, playerID, role):
         self.type = "inrole"
         self.playerID = playerID
         self.role = role
-        self.negative = negative
+        self.negative = negated
