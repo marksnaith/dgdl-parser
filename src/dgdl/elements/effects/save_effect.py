a2b96611df9209bc54047c896962ac851954933a
Mark Snaith <msnaith@dundee.ac.uk>
2020-06-19 16:42:08 +0100

    Bug fix in save effect

--- src/dgdl/elements/effects/save_effect.py
+++ src/dgdl/elements/effects/save_effect.py
@@ -3,5 +3,6 @@ from .effect import Effect
 class SaveEffect(Effect):
 
     def __init__(self, content, variable):
+        self.type = "save"
         self.content = content
         self.variable = variable
