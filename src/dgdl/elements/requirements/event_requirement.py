b5585dff5d87c4da102ec1bcb78e5fbddd4309eb
Mark Snaith <msnaith@dundee.ac.uk>
2020-06-26 14:31:00 +0100

    Adding eventpos to EventRequirement

--- src/dgdl/elements/requirements/event_requirement.py
+++ src/dgdl/elements/requirements/event_requirement.py
@@ -5,6 +5,7 @@ class EventRequirement(Requirement):
     def __init__(self, negated, eventpos, moveID, content=None, user=None):
         self.type = "event"
         self.negated = negated
+        self.eventpos = eventpos
         self.moveID = moveID
         self.content = content
         self.user = user
